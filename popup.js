5808e8b3f647a06dcb795e0c888053596b96e425
Ethan Lebowitz <duckunreal@gmail.com>
2021-04-24 16:42:03 -0600

    toggle now works when tab not focused

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 document.getElementById("ratingsButton").addEventListener("click", toggleRatings);
 
 function toggleRatings(){
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {command: "toggleRatings"}, function(response) {
-			//console.log(response.confirm);
-		});
+	chrome.tabs.query({}, function(tabs) {
+		for (var i=0; i<tabs.length; ++i) {
+			chrome.tabs.sendMessage(tabs[i].id, {command: "toggleRatings"});
+		}
 	});
 }
